1f6fe4ebb31a7539eca4be86e7095d89191b73ac
100249 <100249@0cc19ac9-5ce0-4ca9-ac00-b8016d79ef44>
2014-02-13 11:48:06 +0000

    Added Config/Logging to the server

---
+++ server/util/logger.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import logging
+from util.config import Config
+
+def initialize():
+	messages = []
+	levels = {'NOTSET': logging.NOTSET, 'DEBUG': logging.DEBUG, 'INFO': logging.INFO, 'WARNING': logging.WARNING, 'ERROR': logging.ERROR, 'CRITICAL': logging.CRITICAL}
+	loglevel = Config.get("logging", "severity")
+	logfile = Config.get("logging", "logfile")
+
+	# If the configfile lists a loglevel that is not valid, assume info.
+	if(loglevel not in levels):
+		# Since the logger is not yet initialized, add the logging-message to the messagelist, so that we can
+		#   log it whenever the logger is initialized.
+		messages.append(("LogLevel is not correctly set in the config-file. Assuming INFO", logging.ERROR))
+		print "A"
+		loglevel = "INFO"
+	
+	rootlogger = logging.getLogger()
+	formatter = logging.Formatter('%(asctime)s: %(name)s: %(levelname)s - %(message)s')
+	
+	fh = logging.FileHandler(logfile)
+	fh.setFormatter(formatter)
+	rootlogger.addHandler(fh)
+	rootlogger.setLevel(levels[loglevel])
+	
+	messages.append(("Logger initialized", logging.INFO))
+		
+	# Now that the logger is initialized, log the messages that appared during the initialization of the module 
+	logger = logging.getLogger(__name__)
+	for m in messages:
+		logger.log(m[1], m[0])
