3793e9fc7e9865d95f978ebafcc43c9513ff249f
100249 <100249@0cc19ac9-5ce0-4ca9-ac00-b8016d79ef44>
2014-02-18 16:40:49 +0000

    Updated the xmlrpc-server a bit

--- server/scripts/xmlrpc-testserver.py
+++ server/scripts/xmlrpc-testserver.py
@@ -10,6 +10,7 @@ sys.path.append(parentdir)
 
 # Tell where to find the DJANGO settings.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "srm.settings")
+from core.models import Rule, RuleSet, RuleClass
 from util.xmlrpcserver import RPCServer
 from util.config import Config
 
@@ -17,6 +18,52 @@ class RPCInterface():
 	def __init__(self):
 		import string
 		self.python_string = string
+	
+	def getRuleClasses(self):
+		classes = {}
+		
+		for rc in RuleClass.objects.all():
+			c = {}
+			c['classtype'] = rc.classtype
+			c['description'] = rc.description
+			c['priority'] = rc.priority
+			classes[rc.classtype] = c
+		
+		return classes
+	
+	def getRuleRevisions(self):
+		rulelist = Rule.getRuleRevisions()
+		rl = {}
+		for r in rulelist:
+			rl[str(r)] = rulelist[r]
+		return rl
+	
+	def getRuleSets(self):
+		rulesets = {}
+		for rs in RuleSet.objects.all():
+			ruleset = {}
+			ruleset['name'] = rs.name
+			ruleset['description'] = rs.description 
+			rulesets[rs.name] = ruleset
+		return rulesets
+	
+	def getRules(self, rulelist):
+		if(len(rulelist) > 250):
+			raise Exception("Cannot request more than 250 rules")
+		
+		rules = {}
+		for r in rulelist:
+			rule = Rule.objects.get(SID=r)
+			dictRule = {}
+			dictRule['SID'] = rule.SID
+			dictRule['rev'] = rule.revisions.last().rev
+			dictRule['msg'] = rule.revisions.last().msg
+			dictRule['raw'] = rule.revisions.last().raw
+			dictRule['ruleset'] = rule.ruleSet.name
+			dictRule['ruleclass'] = rule.ruleClass.classtype
+			rules[str(rule.SID)] = dictRule
+		
+		return rules
 	
 	def ping(self):
 		return "Pong!"
