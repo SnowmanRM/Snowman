2a8af0827d25c13cccf13f2aa4ed3cf37db6ee7d
090751 <thnyheim@users.noreply.github.com>
2014-02-27 21:58:14 +0000

    M    src/trunk/server/web/views/ruleviews.py

--- server/web/views/ruleviews.py
+++ server/web/views/ruleviews.py
@@ -22,19 +22,31 @@ def index(request):
 	
 	logger = logging.getLogger(__name__)
 	
+	# Spool up context.
 	context = {}
 	
+	# Get pagelength from the utility class.
 	pagelength = UserSettings.getPageLength(request, pagetype=UserSettings.RULELIST)
 	
+	# This is always page nr 1.
 	context['pagenr'] = 1
+	
+	# We want pagelength with us in the template.
 	context['pagelength'] = pagelength
+	
+	# The first page isnt hidden.
 	context['ishidden'] = False
 	
 	try:
+		# Get the current sensor count, but we want it in a negative value.
 		context['sensorcount'] =  Sensor.objects.count()
 		context['sensorcount'] = -context['sensorcount']
+		
+		# We need to know how many rules there are total.
 		context['itemcount'] = Rule.objects.count()
+		# Get all rules, but limited by the set pagelength.
 		context['rule_list'] = Rule.objects.all()[:pagelength]
+
 	except Rule.DoesNotExist:
 		logger.warning("Page request /rules/ could not be resolved, objects not found.")
 		raise Http404
@@ -55,20 +67,32 @@ def getRulePage(request, pagenr):
 	
 	context = {}
 	
+	# Get pagelength from the utility class.
 	pagelength = UserSettings.getPageLength(request, pagetype=UserSettings.RULELIST)
 	
+	# We want pagenr with us in the template.
 	context['pagenr'] = pagenr
+	
+	# We want pagelength with us in the template.
 	context['pagelength'] = pagelength
+	
+	# The first page isnt hidden.
 	context['ishidden'] = True
 	
+	# We multiply the paglength with the requested pagenr, this should give us the minimum range.
 	minrange = pagelength * int(pagenr)
 	
+	# We add pagelength to the minumum range, this gives us the maximum range.
 	maxrange = int(minrange) + pagelength
 	
 	try:
+		# Get the current sensor count, but we want it in a negative value.
 		context['sensorcount'] =  Sensor.objects.count()
 		context['sensorcount'] = -context['sensorcount']
+		
+		# We need to know how many rules there are total.
 		context['itemcount'] = Rule.objects.count()
+		# Get all rules, within the set range.
 		context['rule_list'] = Rule.objects.all()[minrange:maxrange]
 	except Rule.DoesNotExist:
 		logger.warning("Page request /rules/page/"+str(pagenr)+" could not be resolved, objects in range "+str(minrange)+" - "+str(maxrange)+"not found.")
@@ -91,32 +115,53 @@ def getRulesBySearch(request, pagenr):
 	
 	context = {}
 	
+	# Get the two values from the HTTP POST request.
 	searchstring = request.POST['searchs']
 	searchfield = request.POST['searchf']
 	
+	# Get pagelength from the utility class.
 	pagelength = UserSettings.getPageLength(request, pagetype=UserSettings.RULELIST)
+	
+	# We set this value to true so that we can differentiate in the template.
 	context['rulesearch'] = True
+	
+	# We want pagenr with us in the template, but we modify it.
 	context['pagenr'] = "search"+pagenr
+	
+	# We want pagelength with us in the template.
 	context['pagelength'] = pagelength
+	
+	# The first page isnt hidden.
 	context['ishidden'] = True
+	
+	# We want the searchstring with us in the template.
 	context['searchstring'] = searchstring
 	
+	# If this is the first page or there is only one page, minrange must be 0.
 	if pagenr=='1':
 		minrange=0
 	else:
+		# We multiply the paglength with the requested pagenr, this should give us the minimum range.
 		minrange = pagelength * int(pagenr)
 	
+	# We add pagelength to the minumum range, this gives us the maximum range.
 	maxrange = int(minrange) + pagelength
 	
 	try:
+		# Get the current sensor count, but we want it in a negative value.
 		context['sensorcount'] =  Sensor.objects.count()
 		context['sensorcount'] = -context['sensorcount']
 		
+		# We do different queries based on the searchfield string.
 		if searchfield=='sid':
+			# We need to know how many rules the search will produce.
 			context['itemcount'] = Rule.objects.filter(SID__istartswith=searchstring).count()
+			# Get matching rules, within the set range.
 			context['rule_list'] = Rule.objects.filter(SID__istartswith=searchstring)[minrange:maxrange]
 		elif searchfield=='name':
+			# We need to know how many rules the search will produce.
 			context['itemcount'] = Rule.objects.filter(revisions__active=True, revisions__msg__icontains=searchstring).count()
+			# Get matching rules, within the set range.
 			context['rule_list'] = Rule.objects.filter(revisions__active=True, revisions__msg__icontains=searchstring)[minrange:maxrange]
 
 	except Rule.DoesNotExist:
