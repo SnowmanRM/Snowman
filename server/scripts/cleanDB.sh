175fae2bbea39d7e0e9b4162c615627f87168440
090751 <thnyheim@users.noreply.github.com>
2014-04-10 15:30:43 +0000

    More tests

--- server/scripts/cleanDB.sh
+++ server/scripts/cleanDB.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL="http://192.168.6.11/static/rules/testset.1-1.tar.gz"
+URL="http://192.168.1.100/testing/testset.inactive.100-10.tar.gz"
 
 echo "DROP DATABASE srm; CREATE DATABASE srm;" | mysql -usrm -pbah5oofa6booyeeJa2Da 
 ../manage.py syncdb --noinput
