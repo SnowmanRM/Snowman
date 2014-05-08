60294b0a4b0b33efebebfb7787ca4de92012a7b2
100249 <100249@0cc19ac9-5ce0-4ca9-ac00-b8016d79ef44>
2014-05-08 14:20:39 +0000

    Small changes in the packaging of the client

--- client/package-client.debian.sh
+++ client/package-client.debian.sh
@@ -5,18 +5,18 @@ echo "Using $workdir as a working-directory."
 
 # Create workdir, and copy source there.
 mkdir -p $workdir
-cp -r client/* $workdir
+cp -r * $workdir
 
 # Delete all the subversion files from the source, as we do not want them 
 #   in the final package.
-find /tmp/building/snowman-client/ -name ".svn" -exec rm -rf {} \; &> /dev/null
+find $workdir -name ".svn" -exec rm -rf {} \; &> /dev/null
 
 # Grab some variables from the configfile
 packagename=`grep Package $workdir/DEBIAN/control | cut -d ' ' -f 2`
 packageversion=`grep Version $workdir/DEBIAN/control | cut -d ' ' -f 2`
 
 # Build the package
-fakeroot dpkg -b $workdir $packagename-$packageversion.deb
+fakeroot dpkg -b $workdir ARCHIVES/$packagename-$packageversion.deb
 
 # Clean the working directory
 rm -rf $workdir
